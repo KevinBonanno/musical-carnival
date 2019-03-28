e3c1764d56994893a81df7b57c987915afee2aec
kjb47046@uga.edu <ILJ12bts@@>
2019-03-28 16:16:46 -0400

    Renamed variables and added missing semicolons

--- javascript.js
+++ javascript.js
@@ -1,22 +1,23 @@
-var meme = []
+/*eslint-env browser*/
+var dancesArray = [];
 
-meme[0] = "<iframe src='https://giphy.com/embed/tsVLVcTRKb54mvRS4z'>";
-meme[1] = "<iframe src='https://giphy.com/embed/BcAB7TmA4hJflMObwR'>";
-meme[2] = "<iframe src='https://giphy.com/embed/3olFpXDTYY60NLxF5B'>";
-meme[3] = "<iframe src='https://giphy.com/embed/8qFS8ZXSSiMotzNC0P'>";
+dancesArray[0] = "<iframe src='https://giphy.com/embed/tsVLVcTRKb54mvRS4z'>";
+dancesArray[1] = "<iframe src='https://giphy.com/embed/BcAB7TmA4hJflMObwR'>";
+dancesArray[2] = "<iframe src='https://giphy.com/embed/3olFpXDTYY60NLxF5B'>";
+dancesArray[3] = "<iframe src='https://giphy.com/embed/8qFS8ZXSSiMotzNC0P'>";
 
-function Generate() {
-    var randomMeme = Math.floor(Math.random() * (meme.length));
-    document.getElementById('pmeme').innerHTML = meme[randomMeme];
+function PlayVideo() {
+    var randomMeme = Math.floor(Math.random() * (dancesArray.length));
+    document.getElementById('pmeme').innerHTML = dancesArray[randomMeme];
 }
 
-var audio = []
+var audioArray = [];
 
-audio[0] = "<iframe src='https://drive.google.com/file/d/1gUdOrZTGuBLbKFrnEQkGEQEjyCF1vciV/preview'>";
+audioArray[0] = "<iframe src='https://drive.google.com/file/d/1gUdOrZTGuBLbKFrnEQkGEQEjyCF1vciV/preview'>";
 
 
-function Play() {
-    var randomAudio = Math.floor(Math.random() * (audio.length));
-    document.getElementById('paudio').innerHTML = audio[randomAudio];
+function PlayAudio() {
+    var randomAudio = Math.floor(Math.random() * (audioArray.length));
+    document.getElementById('paudio').innerHTML = audioArray[randomAudio];
 
 }
