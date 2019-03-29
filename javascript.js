52de70c032c3969aeed974073ef7c2cbbe683680
musical-carnival <saranbonanno@gmail.com>
2019-03-29 11:16:03 -0400

    Functional sound + music tag

--- javascript.js
+++ javascript.js
@@ -5,6 +5,10 @@ dancesArray[0] = "<iframe src='https://giphy.com/embed/tsVLVcTRKb54mvRS4z'>";
 dancesArray[1] = "<iframe src='https://giphy.com/embed/BcAB7TmA4hJflMObwR'>";
 dancesArray[2] = "<iframe src='https://giphy.com/embed/3olFpXDTYY60NLxF5B'>";
 dancesArray[3] = "<iframe src='https://giphy.com/embed/8qFS8ZXSSiMotzNC0P'>";
+dancesArray[4] = "<iframe src='https://giphy.com/embed/7FgZeNcxdL8URqQi2X'>";
+dancesArray[5] = "<iframe src='https://giphy.com/embed/bqGaIoo2sN6KparUwM'>";
+dancesArray[6] = "<iframe src='https://giphy.com/embed/1o1fMfYGvnibozOTjn'>";
+dancesArray[7] = "<iframe src='https://giphy.com/embed/pcJQpEUNwyn6LtxtPO'>";
 
 function PlayVideo() {
     var randomMeme = Math.floor(Math.random() * (dancesArray.length));
@@ -13,7 +17,14 @@ function PlayVideo() {
 
 var audioArray = [];
 
-audioArray[0] = "<iframe src='https://drive.google.com/file/d/1gUdOrZTGuBLbKFrnEQkGEQEjyCF1vciV/preview'>";
+audioArray[0] = "Butterflies by Kacey Musgraves<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1UZ38ucPo92_nHNJ6z6obHKdKXwrr2Wql' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1UZ38ucPo92_nHNJ6z6obHKdKXwrr2Wql' type='audio/mpeg'>;</audio>";
+audioArray[1] = "Dreams by Fleetwood Mac<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1d14k1JmSw8J5yLp_qmmJfEA4tfz3aVHQ' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1d14k1JmSw8J5yLp_qmmJfEA4tfz3aVHQ' type='audio/mpeg'>;</audio>";
+audioArray[2] = "Everyday by Weyes Blood<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1mmQUwObKt1R66xiZf1WcM6P56GdGSbq3' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1mmQUwObKt1R66xiZf1WcM6P56GdGSbq3' type='audio/mpeg'>;</audio>";
+audioArray[3] = "High by the Beach by Lana Del Rey<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1fLSjFXpIZSGjRNnKx9KJ5crNm_1F2mt2' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1fLSjFXpIZSGjRNnKx9KJ5crNm_1F2mt2' type='audio/mpeg'>;</audio>";
+audioArray[4] = "It's too late by Carole King<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=19tAkzUI040XfK7G2zaeuNjmvCtM2ofG9' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=19tAkzUI040XfK7G2zaeuNjmvCtM2ofG9' type='audio/mpeg'>;</audio>";
+audioArray[5] = "Easy Easy by King Krule<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1BW3Y1eOepaNq0a4XBtHVpeVIwyyQh0zg' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1BW3Y1eOepaNq0a4XBtHVpeVIwyyQh0zg' type='audio/mpeg'>;</audio>";
+audioArray[6] = "Chamber of Reflection by Mac Demarco<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=1btfx8eGhkriZauJkHB21iAi5NNPJEoar' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=1btfx8eGhkriZauJkHB21iAi5NNPJEoar' type='audio/mpeg'>;</audio>";
+audioArray[7] = "Death with Dignity by Sufjan Stevens<audio autoplay loop><iframe src='https://drive.google.com/uc?export=view&id=184CnBLtJiRhjuRIgg9DwZv-7hl4Gtubt' type='audio/ogg'>;<iframe src='https://drive.google.com/uc?export=view&id=184CnBLtJiRhjuRIgg9DwZv-7hl4Gtubt' type='audio/mpeg'>;</audio>";
 
 
 function PlayAudio() {
@@ -21,3 +32,4 @@ function PlayAudio() {
     document.getElementById('paudio').innerHTML = audioArray[randomAudio];
 
 }
+
